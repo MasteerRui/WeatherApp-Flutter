cd2aa6bb67e5035b84957ef9a138dd7cb1389668
Rui Florêncio <75584975+MasteerRui@users.noreply.github.com>
2024-06-10 12:26:24 +0200

    Update dart_service.dart

--- weather/lib/service/dart_service.dart
+++ weather/lib/service/dart_service.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 class DataService {
   Future<WeatherResponse> getWeather(long, lat) async {
     final response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?&lon=$long&lat=$lat&appid=d89566b7541ecad9d211291d677951ba&units=metric'));
+        'https://api.openweathermap.org/data/2.5/weather?&lon=$long&lat=$lat&appid=Add Here&units=metric'));
     final json = jsonDecode(response.body);
     return WeatherResponse.fromJson(json);
   }
